bf56c39c12e667cd10c0aee1b4181b0a3a64a510
chrisMJ <chris666.10@hotmail.com>
2024-03-14 18:03:07 -0500

    corrección de modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { AutorModule } from './autor/autor.module';
+import { LibroModule } from './libro/libro.module';
+import { EscribeModule } from './escribe/escribe.module'
 @Module({
-  imports: [],
+  imports: [AutorModule, LibroModule, EscribeModule],
   controllers: [AppController],
   providers: [AppService],
 })
